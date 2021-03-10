288eab89ca88ec0a28dd860a31710ca2ba27bec6
Pharez Varlack <varlack17@hotmail.com>
2021-03-10 11:35:05 -0500

    Added Resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -2,11 +2,15 @@
 import resume from './resume.pdf';
 import PDF from 'react-pdf-js';
 import '../App.css';
+import {Document, Page, pdfjs} from 'react-pdf';
+import Container from 'react-bootstrap/Container';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 
 function Resume() {
 	return (
-		<div>
-			<h1> Coming Soon!</h1>
+		<div style={{backgroundColor: '#E6E6E6'}}>
+			<iframe src="./resume.pdf" className='Pdf'></iframe>
 		</div>
 		)
 }
