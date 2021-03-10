a8b9677c29eff744eabae448c45fa373c45ac5c1
Pharez Varlack <varlack17@hotmail.com>
2021-03-10 18:58:27 -0500

    Removed unused libraries

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,11 +1,6 @@
 //import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, Annotation, TextSearch, Inject } from '@syncfusion/ej2-react-pdfviewer';
-import resume from './resume.pdf';
-import PDF from 'react-pdf-js';
 import '../App.css';
-import {Document, Page, pdfjs} from 'react-pdf';
 import Container from 'react-bootstrap/Container';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
 
 function Resume() {
 	return (
